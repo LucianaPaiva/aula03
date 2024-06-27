68017eeaa2c5b943cdc7f41b3529ac1631a3aca0
Luciana Paiva <143903142+LucianaPaiva@users.noreply.github.com>
2024-06-27 11:23:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+// var contador = 17
+
+
+// while (contador > 0) {
+//     console.log(contador)
+//     contador --
+// }
+// ---------------------------------------------------------------------
+// let contador = 0
+
+// while (contador < 10) {
+//     console.log(contador)
+//     if (contador === 5){
+//         break
+//     }
+//     contador++
+// }
+// console.log(`O laço foi quebrado e o contador parou com o valor ${contador}`)
+
+// ----------------------------------------------------------------------------01
+
+// Atv01
+// var num = prompt('Digite um número')
+    
+//     while (num > 0) {
+//         console.log(num)
+//         num --
+        
+//     }
+
+//     --------------------------------------------------------------02
+
+// Atv02
+
+notas = []
+continuar = True
+
+while nota_str = input("Insira uma nota (ou digite qualquer letra para parar): "){
+    if nota_str.isnumeric():
+    nota = int(nota_str)
+}
+    
+//     if nota_str.strip():  # Verifica se a string não está vazia ou contém apenas espaços
+//         try:
+//             nota = float(nota_str)
+//             notas.append(nota)
+//         except ValueError:
+//             continuar = False
+//     else:
+//         continuar = False
+
+// if len(notas) > 0:
+//     media = sum(notas) / len(notas)
+//     print(f"A média das notas inseridas é: {media:.2f}")
+// else:
+//     print("Nenhuma nota válida foi inserida.")
